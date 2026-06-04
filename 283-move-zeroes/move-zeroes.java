b1698d7f9711adfb4d7c7e832868bdad25b15fd8
BitByBit <72179769+tiwariraju@users.noreply.github.com>
2026-06-04 12:16:33 +0530

    Time: 3 ms (18.60%) | Memory: 47.6 MB (74.12%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+
+        // Edge case
+        if(j==-1) return;
+
+        // final
+        for(int i=j+1;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]= temp;
+                j++;
+            }
+        }
+    }
+}
